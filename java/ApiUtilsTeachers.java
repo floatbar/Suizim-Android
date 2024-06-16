0cb37162eb8fbb2827315c225d86b80bf6c1dc4e
floatbar <150714882+floatbar@users.noreply.github.com>
2024-06-16 23:19:54 +0300

    Update ApiUtilsTeachers.java

--- java/ApiUtilsTeachers.java
+++ java/ApiUtilsTeachers.java
@@ -2,8 +2,8 @@ package wafoot.becoming.wafoot;
 
 public class ApiUtilsTeachers {
 
-    private static final String FIRST_BASE_URL = "https://0.0.0.0:1121/"; // Replace with your actual server URL
-    private static final String SECOND_BASE_URL = "https://0.0.0.0:4338"; // Replace with your actual server URL
+    private static final String FIRST_BASE_URL = "https://0.0.0.0:6666/"; // Replace with your actual server URL.
+    private static final String SECOND_BASE_URL = "https://0.0.0.0:6667"; // Replace with your actual server URL.
 
     public static TeachersInterface getTeachersInterface() {
         return RetrofitClient.getClient(FIRST_BASE_URL).create(TeachersInterface.class);
