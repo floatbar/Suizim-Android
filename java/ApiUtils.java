1219ec4c21a2696f1f66523f4b260e262e0aa9d6
floatbar <150714882+floatbar@users.noreply.github.com>
2024-06-16 22:18:58 +0300

    Create ApiUtils.java

---
+++ java/ApiUtils.java
@@ -0,0 +1,15 @@
+package wafoot.becoming.wafoot;
+
+public class ApiUtils {
+
+    private static final String FIRST_BASE_URL = "https://0.0.0.0:1121/"; // Replace with your actual server URL
+    private static final String SECOND_BASE_URL = "https://0.0.0.0:4338"; // Replace with your actual server URL
+
+    public static StudentsInterface getStudentsInterface() {
+        return RetrofitClient.getClient(FIRST_BASE_URL).create(StudentsInterface.class);
+    }
+
+    public static StudentsInterface getNodejsStudentsInterface() {
+        return RetrofitClient.getClient(SECOND_BASE_URL).create(StudentsInterface.class); // I will not share this app's backend codes :-)
+    }
+}
