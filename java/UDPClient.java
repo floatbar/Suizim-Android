28c1a3fbc3df24b4de18b3c39c0bbd18914b9abd
floatbar <150714882+floatbar@users.noreply.github.com>
2024-06-17 01:03:58 +0300

    Update UDPClient.java

--- java/UDPClient.java
+++ java/UDPClient.java
@@ -15,7 +15,8 @@ import java.net.InetAddress;
 // The UDP server is ready and running at the moment.
 // However, for the server's security, unfortunately, Suizim's UDP server isn't used only for now.
 // Instead, Firebase is now being used for message operations.
-// Even so, do not worry. Soon, I will delete my Firebase database, and will replace Firebase with UDP for message operations within the app.
+// Even so, do not worry. Soon, I will delete my Firebase database, and will replace Firebase with my UDP server for message operations within the app.
+// Do not forget that UDP transfers data much faster than other protocols :-)
 
 public class UDPClient {
 
