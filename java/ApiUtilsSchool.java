537b370ecfc67ed16065e6ad2afbc171731310cd
floatbar <150714882+floatbar@users.noreply.github.com>
2024-06-16 23:18:50 +0300

    Update ApiUtilsSchool.java

--- java/ApiUtilsSchool.java
+++ java/ApiUtilsSchool.java
@@ -2,7 +2,7 @@ package wafoot.becoming.wafoot;
 
 public class ApiUtilsSchool {
 
-    private static final String BASE_URL = "https://0.0.0.0:4338"; // Replace with your actual server URL
+    private static final String BASE_URL = "https://0.0.0.0:6666"; // Replace with your actual server URL.
 
     public static SchoolInterface getSchoolInterface() {
         return RetrofitClient.getClient(BASE_URL).create(SchoolInterface.class);
