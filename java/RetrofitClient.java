72b21ad567ae70b33c512c99427a97ddb18ba1d5
floatbar <150714882+floatbar@users.noreply.github.com>
2024-06-17 03:45:40 +0300

    Update RetrofitClient.java

--- java/RetrofitClient.java
+++ java/RetrofitClient.java
@@ -7,9 +7,9 @@ public class RetrofitClient {
     
     private static Retrofit retrofit = null;
 
-    public static Retrofit getClient(String baseurl) {
+    public static Retrofit getClient(String baseUrl) {
         if (retrofit == null) {
-            retrofit = new Retrofit.Builder().baseUrl(baseurl).addConverterFactory(GsonConverterFactory.create()).build();
+            retrofit = new Retrofit.Builder().baseUrl(baseUrl).addConverterFactory(GsonConverterFactory.create()).build();
         }
 
         return retrofit;
