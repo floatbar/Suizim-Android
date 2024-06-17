666e6905060a74c43fb5de534c74a591259c6e42
floatbar <150714882+floatbar@users.noreply.github.com>
2024-06-17 03:44:56 +0300

    Update RetrofitClient.java

--- java/RetrofitClient.java
+++ java/RetrofitClient.java
@@ -2,7 +2,9 @@ package wafoot.becoming.wafoot;
 
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+
 public class RetrofitClient {
+    
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient(String baseurl) {
