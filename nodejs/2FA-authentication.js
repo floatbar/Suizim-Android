11df201db5a7c152156bba80957d8cd0d5e4ed0e
floatbar <150714882+floatbar@users.noreply.github.com>
2024-07-13 21:21:42 +0300

    Create 2FA-authentication.js

---
+++ nodejs/2FA-authentication.js
@@ -0,0 +1,16 @@
+// The file, in which APIs are written, (index.js) has not been included in the repository due to security considerations.
+
+const crypto = require("crypto");
+
+function generate2FAPassword(secret, password) { 
+    return crypto.createHmac("sha256", secret).update(password).digest("hex");
+}
+
+function verify2FAPassword(secret, password, twoFAPassword) {
+    return twoFAPassword === crypto.createHmac("sha256", secret).update(password).digest("hex");
+}
+
+module.exports = {
+    generate2FAPassword,
+    verify2FAPassword
+};
