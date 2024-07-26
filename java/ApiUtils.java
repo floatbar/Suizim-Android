511f808fd760d5a5116b211083b2a3be3bc4d391
floatbar <150714882+floatbar@users.noreply.github.com>
2024-07-26 15:19:34 +0300

    Update ApiUtils.java

--- java/ApiUtils.java
+++ java/ApiUtils.java
@@ -2,7 +2,7 @@ package wafoot.becoming.wafoot;
 
 public class ApiUtils {
 
-    private static final String FIRST_BASE_URL = "https://0.0.0.0:6666/"; // Replace with your actual server URL.
+    private static final String FIRST_BASE_URL = "https://0.0.0.0:6666/"; // Replace with your actual server URL
     private static final String SECOND_BASE_URL = "https://0.0.0.0:6667"; // Replace with your actual server URL.
 
     public static StudentsInterface getStudentsInterface() {
