3daeacae269877107df8e0084edcae2eb8c98d69
floatbar <150714882+floatbar@users.noreply.github.com>
2025-02-21 16:31:36 +0300

    Update connection-protocol.js

--- nodejs/connection-protocol.js
+++ nodejs/connection-protocol.js
@@ -1,7 +1,7 @@
 // The file, in which secure APIs are written, (main.js) has not been included in the repository due to security considerations.
 
-const net = require("net"); // TCP server creator for handling the Suizim clients' or any other client's connections
+const net = require("net"); // In order to handle the Suizim clients' or any other client's connections
 require("dotenv").config();
 
-const server = net.createServer((socket) => {});
+const server = net.createServer((socket) => socket.write(Buffer.from("121", "utf-8")));
 server.listen(process.env.CUSTOM_PROTOCOL_PORT, process.env.CLOUD_SERVER_HOST, () => console.log("TCP server running..."));
